adf90112ed0af5f4a820e1e682b52310fe3c7d60
GayathriSK148 <59767463+GayathriSK148@users.noreply.github.com>
2021-04-18 16:49:10 +0530

    Create SumInverse.py

---
+++ SumInverse.py
@@ -0,0 +1,18 @@
+def SumInverse(sum, numberLength):
+    if sum == 0:
+        return 1
+    if sum == 0:
+        return 0
+    n = 10**numberLength
+    numbers = []
+    counter = 0
+    for i in range(0,n):
+        x = 0
+        for digit in str(i):
+            x = x + int(digit)
+        if x == sum:
+            numbers.append(str(i))
+            counter += 1
+    print(numbers)
+    print(counter)
+fun1(7, 2)
